bf41f3b099b27cf48781de57dbcdca73f67018d6
armaganakyrt <akyurta@mef.edu.tr>
2021-04-14 00:46:52 +0300

    Four connected labeling
    4-connected labeling for tetris grid.

--- game_grid.py
+++ game_grid.py
@@ -186,25 +186,33 @@ class GameGrid:
       return score
 
 
-   def four_connected(self):
+   def __four_connected(self):
       height = self.grid_height
       width= self.grid_width
-      labeled_grid = np.zeros(shape=(height,width))
+      labeled_grid = np.zeros(shape=(height+2, width+2))
+      new_tile_matrix = np.full(shape=(height+2 ,width+2), fill_value=None)
+      for col in range(width):
+         for row in range(height):
+            new_tile_matrix[row+1][col+1] = self.tile_matrix[row][col]
+
+      height += 2
+      width += 2
+
       k = 0
       for i in range(1,height-1):
          for j in range(1, width-1):
-            if self.tile_matrix[i][j] is not None:
-               if self.tile_matrix[i-1][j] is None and self.tile_matrix[i][j - 1] is None:
+            if new_tile_matrix[i][j] is not None:
+               if new_tile_matrix[i-1][j] is None and new_tile_matrix[i][j - 1] is None:
                   k += 1
                   labeled_grid[i][j] = k
 
-               elif self.tile_matrix[i - 1][j] is not None and self.tile_matrix[i][j - 1] is None:
+               elif new_tile_matrix[i - 1][j] is not None and new_tile_matrix[i][j - 1] is None:
                   labeled_grid[i][j] = labeled_grid[i - 1][j]
 
-               elif self.tile_matrix[i - 1][j] is None and self.tile_matrix[i][j - 1] is not None:
+               elif new_tile_matrix[i - 1][j] is None and new_tile_matrix[i][j - 1] is not None:
                   labeled_grid[i][j] = labeled_grid[i][j - 1]
 
-               elif self.tile_matrix[i - 1][j] is not None and self.tile_matrix[i][j - 1] is not None:
+               elif new_tile_matrix[i - 1][j] is not None and new_tile_matrix[i][j - 1] is not None:
                   if labeled_grid[i - 1][j] == labeled_grid[i][j - 1]:
                      labeled_grid[i][j] = labeled_grid[i][j - 1]
 
@@ -218,12 +226,15 @@ class GameGrid:
 
 
    def __change_label(self,label ,up_lbl, left_lbl, i, j):
-      for x in range(i-1):
+      for x in range(i):
          for y in range(j):
             if label[x][y] == left_lbl:
                label[x][y] = up_lbl
 
 
-      for n in range(j-1):
+      for n in range(j):
          if label[i][n] == left_lbl:
             label[i][n] = up_lbl
+
+
+
